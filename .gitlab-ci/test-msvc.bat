c0c100e3941c9845f5870196d22112ba4cb220bc
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-09-13 20:01:28 -0700

    Update 'test-msvc.bat' to use VS2022
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- .gitlab-ci/test-msvc.bat
+++ .gitlab-ci/test-msvc.bat
@@ -1,14 +1,38 @@
-@echo on
-:: vcvarsall.bat sets various env vars like PATH, INCLUDE, LIB, LIBPATH for the
-:: specified build architecture
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
-@echo on
+@echo off
+goto:$main
 
-:: FIXME: make warnings fatal
-pip3 install --upgrade --user meson~=0.64 || goto :error
-meson setup -Dbackend_max_links=1 -Ddebug=false -Dmedia-gstreamer=disabled -Dvulkan=disabled _build || goto :error
-ninja -C _build || goto :error
-
-goto :EOF
-:error
+:$error
 exit /b 1
+
+:$build
+setlocal EnableExtensions
+    set "SOURCE_DIR=%~1"
+    :: remove trailing slash if there is one
+    if "%SOURCE_DIR:~-1%"=="\" set "SOURCE_DIR=%SOURCE_DIR:~0,-1%"
+
+    rye run meson setup ^
+        -Dbackend_max_links=1 ^
+        -Ddebug=false ^
+        -Dmedia-gstreamer=disabled ^
+        -Dvulkan=disabled "%SOURCE_DIR%\_build" "%SOURCE_DIR%"
+    if errorlevel 1 goto :$build_error
+
+    ninja -C _build
+    if errorlevel 1 goto :$build_error
+
+    :$build_error
+    echo Build error.
+    goto:$build_done
+
+    :$build_done
+endlocal & exit /b %ERRORLEVEL%
+
+:$main
+setlocal EnableExtensions
+    :: vcvarsall.bat sets various env vars like PATH, INCLUDE, LIB, LIBPATH for the
+    :: specified build architecture
+    call "C:\Program Files\Microsoft Visual Studio\2022\Preview\VC\Auxiliary\Build\vcvarsall.bat" x64
+    call :$build "%~dp0\..\"
+endlocal & (
+    exit /b %ERRORLEVEL%
+)
